8899e95753f4a3ab0250aa9843815722a9fbf309
Andrea Barisani <andrea@inversepath.com>
2025-03-04 12:06:13 +0100

    show detected frequency

--- cmd/amd64.go
+++ cmd/amd64.go
@@ -53,6 +53,7 @@ func infoCmd(_ *shell.Interface, _ []string) (string, error) {
 	fmt.Fprintf(&res, "Runtime ......: %s %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 	fmt.Fprintf(&res, "RAM ..........: %#08x-%#08x (%d MiB)\n", ramStart, ramEnd, (ramEnd-ramStart)/(1025*1024))
 	fmt.Fprintf(&res, "CPU ..........: %s\n", efi.AMD64.Name())
+	fmt.Fprintf(&res, "Frequency ....: %v GHz\n", float32(efi.AMD64.Freq())/1e9)
 
 	return res.String(), nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/hako/durafmt v0.0.0-20210608085754-5c1018a4e16b
 	github.com/u-root/u-root v0.14.1-0.20250211171033-17c75c6542a1
 	github.com/usbarmory/armory-boot v0.0.0-20250207173330-3cfbfb527102
-	github.com/usbarmory/tamago v0.0.0-20250303160120-fff670ad0693
+	github.com/usbarmory/tamago v0.0.0-20250304104809-386821b87ee4
 	golang.org/x/term v0.29.0
 )
 
