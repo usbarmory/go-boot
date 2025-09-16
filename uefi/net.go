3aefc56d9f0b043f7aa91c14236b816f6fdc5140
Andrea Barisani <andrea@inversepath.com>
2025-09-16 22:52:19 +0200

    add EFI_SIMPLE_NETWORK.GetStatus() support

--- uefi/net.go
+++ uefi/net.go
@@ -8,6 +8,8 @@ package uefi
 const (
 	EFI_SIMPLE_NETWORK_PROTOCOL_GUID     = "a19832b9-ac25-11d3-9a2d-0090273fc14d"
 	EFI_SIMPLE_NETWORK_PROTOCOL_REVISION = 0x00010000
+
+	EFI_SIMPLE_NETWORK_TRANSMIT_INTERRUPT = 0x02
 )
 
 // EFI Simple Network Protocol offsets
@@ -15,6 +17,7 @@ const (
 	start      = 0x08
 	stop       = 0x10
 	initialize = 0x18
+	getStatus  = 0x58
 	transmit   = 0x60
 	receive    = 0x68
 )
@@ -59,8 +62,27 @@ func (sn *SimpleNetwork) Initialize() (err error) {
 	return parseStatus(status)
 }
 
-// Transmit calls EFI_SIMPLE_NETWORK.Transmit()
+// GetStatus calls EFI_SIMPLE_NETWORK.GetStatus()
+func (sn *SimpleNetwork) GetStatus() (interruptStatus uint32, txBuf uint64, err error) {
+	status := callService(sn.base+getStatus,
+		[]uint64{
+			sn.base,
+			ptrval(&interruptStatus),
+			ptrval(&txBuf),
+		},
+	)
+
+	err = parseStatus(status)
+
+	return
+}
+
+// Transmit calls EFI_SIMPLE_NETWORK.Transmit(), the function waits for
+// EFI_SIMPLE_NETWORK.GetStatus() to report a transmit interrupt before
+// returning.
 func (sn *SimpleNetwork) Transmit(buf []byte) (err error) {
+	var interruptStatus uint32
+
 	status := callService(sn.base+transmit,
 		[]uint64{
 			sn.base,
@@ -73,7 +95,21 @@ func (sn *SimpleNetwork) Transmit(buf []byte) (err error) {
 		},
 	)
 
-	return parseStatus(status)
+	if err = parseStatus(status); err != nil {
+		return
+	}
+
+	for {
+		if interruptStatus, _, err = sn.GetStatus(); err != nil {
+			return
+		}
+
+		if interruptStatus & EFI_SIMPLE_NETWORK_TRANSMIT_INTERRUPT != 0 {
+			break
+		}
+	}
+
+	return
 }
 
 // Receive calls EFI_SIMPLE_NETWORK.Receive()
