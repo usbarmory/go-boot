e40334e1f7a7ff529c1b899e9bbe0ee15f4b793a
Andrea Barisani <andrea@inversepath.com>
2025-09-17 17:51:40 +0200

    disable escape analysis

--- uefi/uefi.go
+++ uefi/uefi.go
@@ -26,6 +26,8 @@ const signature = 0x5453595320494249 // TSYS IBI
 var mux sync.Mutex
 
 // defined in uefi.s
+//
+//go:noescape
 func callFn(fn uint64, n int, args []uint64) (status uint64)
 
 // callService calls an UEFI service
