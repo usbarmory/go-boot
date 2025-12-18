7c63993a8aa340c630b4b4d8db1900b0d22074ad
Andrea Barisani <andrea@inversepath.com>
2025-12-18 23:16:41 +0100

    add missing files

---
+++ cmd/sev.go
@@ -0,0 +1,55 @@
+// Copyright (c) The go-boot authors. All Rights Reserved.
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+
+	"github.com/usbarmory/tamago/kvm/svm"
+
+	"github.com/usbarmory/go-boot/shell"
+	"github.com/usbarmory/go-boot/uefi"
+	"github.com/usbarmory/go-boot/uefi/x64"
+)
+
+func init() {
+	shell.Add(shell.Cmd{
+		Name: "sev",
+		Help: "AMD SEV-SNP information",
+		Fn:   sevCmd,
+	})
+}
+
+func sevCmd(_ *shell.Interface, _ []string) (res string, err error) {
+	var buf bytes.Buffer
+	var snp *uefi.SNPConfigurationTable
+
+	if !x64.AMD64.Features().SNP {
+		return "", errors.New("AMD SEV-SNP unavailable")
+	}
+
+	if snp, _ = x64.UEFI.GetSNPConfiguration(); snp != nil {
+		return "", errors.New("could find AMD SEV-SNP pages")
+	}
+
+	fmt.Fprintf(&buf, "Version ............: %d\n", snp.Version)
+	fmt.Fprintf(&buf, "Secrets Page .......: %x (%d)\n", snp.SecretsPagePhysicalAddress, snp.SecretsPageSize)
+	fmt.Fprintf(&buf, "  CPUID Page .......: %x (%d)\n", snp.CPUIDPagePhysicalAddress, snp.CPUIDPageSize)
+
+	if snp.Version != 2 {
+		return
+	}
+
+	ghcb := &svm.GHCB{}
+	x64.AllocateDMA(svm.GHCBSize)
+
+	ghcb.Init()
+	ghcb.Yield()
+
+	return buf.String(), nil
+}
---
+++ uefi/sev.go
@@ -0,0 +1,53 @@
+// Copyright (c) The go-boot authors. All Rights Reserved.
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package uefi
+
+import (
+	"errors"
+)
+
+const snpSignature = 0x45444d41
+
+// AMD SEV-ES Guest-Hypervisor Communication Block Standardization
+var EFI_SEV_SNP_CC_BLOB_GUID = MustParseGUID("067b1f5f-cf26-44c5-8554-93d777912d42")
+
+// SNPConfigurationTable represents an EFI SNP Confidential Computing Blob
+// Configuration Table.
+type SNPConfigurationTable struct {
+	Header                     uint32
+	Version                    uint16
+	_                          uint16
+	SecretsPagePhysicalAddress uint64
+	SecretsPageSize            uint32
+	_                          uint32
+	CPUIDPagePhysicalAddress   uint64
+	CPUIDPageSize              uint32
+	_                          uint32
+}
+
+// GetSNPConfiguration returns the EFI SNP Confidential Computing Blob
+// Configuration Table.
+func (s *Services) GetSNPConfiguration() (snp *SNPConfigurationTable, err error) {
+	var t *ConfigurationTable
+
+	if s.SystemTable == nil {
+		return nil, errors.New("EFI System Table is invalid")
+	}
+
+	if t, err = s.SystemTable.LocateConfiguration(EFI_SEV_SNP_CC_BLOB_GUID); err != nil {
+		return
+	}
+
+	if err = decode(&snp, t.VendorTable); err != nil {
+		return nil, err
+	}
+
+	if snp.Header != snpSignature {
+		return nil, errors.New("EFI SNP Configuration Table is invalid")
+	}
+
+	return
+}
