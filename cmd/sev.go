765b583bc3964dca582c4171ca61e0e212d345cc
Andrea Barisani <andrea@inversepath.com>
2025-12-28 17:03:25 +0100

    split SEV-SNP commands and add only when features are present

--- cmd/sev.go
+++ cmd/sev.go
@@ -8,6 +8,7 @@ package cmd
 import (
 	"bytes"
 	"crypto/rand"
+	"errors"
 	"fmt"
 
 	"github.com/usbarmory/tamago/dma"
@@ -18,11 +19,23 @@ import (
 	"github.com/usbarmory/go-boot/uefi/x64"
 )
 
+var vmpck0 []byte
+
 func init() {
+	if !svm.Features(x64.AMD64).SEV.SEV {
+		return
+	}
+
 	shell.Add(shell.Cmd{
 		Name: "sev",
 		Help: "AMD SEV-SNP information",
 		Fn:   sevCmd,
+	})
+
+	shell.Add(shell.Cmd{
+		Name: "sev-report",
+		Help: "AMD SEV-SNP attestation report",
+		Fn:   attestationCmd,
 	})
 
 	// DMA region must be allocated before GHCB initialization
@@ -31,10 +44,7 @@ func init() {
 
 func sevCmd(_ *shell.Interface, _ []string) (res string, err error) {
 	var buf bytes.Buffer
-	var key []byte
-
 	var snp *uefi.SNPConfigurationTable
-	var report *svm.AttestationReport
 
 	defer func() {
 		res = buf.String()
@@ -57,24 +67,35 @@ func sevCmd(_ *shell.Interface, _ []string) (res string, err error) {
 	}
 
 	fmt.Fprintf(&buf, "Revision ...........: %d\n", snp.Version)
-	fmt.Fprintf(&buf, "Secrets Page .......: %x (%d)\n", snp.SecretsPagePhysicalAddress, snp.SecretsPageSize)
-	fmt.Fprintf(&buf, "  CPUID Page .......: %x (%d)\n", snp.CPUIDPagePhysicalAddress, snp.CPUIDPageSize)
+	fmt.Fprintf(&buf, "Secrets Page .......: %#x (%d bytes)\n", snp.SecretsPagePhysicalAddress, snp.SecretsPageSize)
+	fmt.Fprintf(&buf, "CPUID Page .........: %#x (%d bytes)\n", snp.CPUIDPagePhysicalAddress, snp.CPUIDPageSize)
 
 	secrets := &svm.SNPSecrets{
 		Address: uint(snp.SecretsPagePhysicalAddress),
 		Size:    int(snp.SecretsPageSize),
 	}
 
-	fmt.Fprintf(&buf, "Attestation Report:\n")
-
 	if err = secrets.Init(); err != nil {
 		fmt.Fprintf(&buf, " could not initialize AMD SEV-SNP secrets, %v", err)
 		return
 	}
 
-	if key, err = secrets.VMPCK(0); err != nil {
+	if vmpck0, err = secrets.VMPCK(0); err != nil {
 		fmt.Fprintf(&buf, " could not get VMPCK0, %v", err)
 		return
+	}
+
+	n := len(vmpck0)
+	fmt.Fprintf(&buf, "VMPCK0 .............: %x -- %x (%d bytes)\n", vmpck0[0], vmpck0[n-1], n)
+
+	return
+}
+
+func attestationCmd(_ *shell.Interface, _ []string) (res string, err error) {
+	var report *svm.AttestationReport
+
+	if len(vmpck0) == 0 {
+		return "", errors.New("AMD SEV-SNP secrsts unavailable, run `sev` first")
 	}
 
 	ghcb := &svm.GHCB{
@@ -82,18 +103,15 @@ func sevCmd(_ *shell.Interface, _ []string) (res string, err error) {
 	}
 
 	if err = ghcb.Init(); err != nil {
-		fmt.Fprintf(&buf, " could not initialize GHCB, %v", err)
-		return
+		return "", fmt.Errorf("could not initialize GHCB, %v", err)
 	}
 
 	data := make([]byte, 64)
 	rand.Read(data)
 
-	if report, err = ghcb.GetAttestationReport(data, key, 0); err != nil {
-		fmt.Fprintf(&buf, " could not get report, %v", err)
-		return
+	if report, err = ghcb.GetAttestationReport(data, vmpck0, 0); err != nil {
+		return "", fmt.Errorf("could not get report, %v", err)
 	}
 
-	fmt.Fprintf(&buf, "%+v", report)
-	return
+	return fmt.Sprintf("%+v", report), nil
 }
