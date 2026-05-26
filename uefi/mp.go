bf241f1068eb37f716936b73e978a29e3db9632c
Andrea Barisani <andrea@inversepath.com>
2026-05-26 15:38:15 +0200

    initial support for EFI Multi Processor Services instance

---
+++ uefi/mp.go
@@ -0,0 +1,41 @@
+// Copyright (c) The go-boot authors. All Rights Reserved.
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package uefi
+
+var EFI_MP_SERVICES_PROTOCOL_GUID = MustParseGUID("3fdda605-a76e-4f46-ad29-12f4531b3d08")
+
+// EFI Multi Processor Services Protocol offsets
+const (
+	getNumberOfProcessors = 0x00
+)
+
+// MultiProcessor represents an EFI Multi Processor Services instance.
+type MultiProcessor struct {
+	base uint64
+}
+
+// GetNumberOfProcessors calls EFI_MP_SERVICES.GetNumberOfProcessors().
+func (mp *MultiProcessor) GetNumberOfProcessors() (num uint64, enabled uint64, err error) {
+	status := callService(mp.base+getNumberOfProcessors,
+		[]uint64{
+			mp.base,
+			ptrval(&num),
+			ptrval(&enabled),
+		},
+	)
+
+	err = parseStatus(status)
+
+	return
+}
+
+// GetMultiProcessor locates and returns the EFI Multi Processor Services
+// instance.
+func (s *BootServices) GetMultiProcessor() (mp *MultiProcessor, err error) {
+	mp = &MultiProcessor{}
+	mp.base, err = s.LocateProtocol(EFI_MP_SERVICES_PROTOCOL_GUID)
+	return
+}
